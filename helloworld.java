e3704b6493dd555a96753208175afad7ddcdf000
shanmaa <51195592+shanmaa@users.noreply.github.com>
2022-11-08 02:23:12 -0800

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - 8th November 2022 - v4 - 4PM ");
+    System.out.println("Hello World - 8th November 2022 - every minute - 4PM ");
     }
 }
